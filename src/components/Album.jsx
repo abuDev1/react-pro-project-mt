da16ea478461194bfdd83839e7783d33e4696cc5
Khusein <Khusein@gmail.com>
2024-09-28 23:59:48 +0300

    fixed some problems

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectId } from "../redux/actions";
 
 
 export const Album = ({ album }) => {
+
+    const selectedAlbumId = useSelector((state) => state.albums.selectedAlbumId)
 
     const dispatch = useDispatch()
     const selectAlbumId = () => {
@@ -12,7 +14,7 @@ export const Album = ({ album }) => {
 
     
     return (
-      <li onClick={selectAlbumId} className={selectAlbumId === album.id ? 'selected' : ''}>
+      <li onClick={selectAlbumId} className={selectedAlbumId === album.id ? 'selected' : ''}>
         <div className="album-name">{album.title} </div>
       </li>
     )
